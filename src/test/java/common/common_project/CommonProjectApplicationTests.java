42512d38afe619c1464f5e60511d34f6f8af6b9d
sungwoong-Noh <asdz453@gmail.com>
2024-06-24 15:11:11 +0900

    first commit

---
+++ src/test/java/common/common_project/CommonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package common.common_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
